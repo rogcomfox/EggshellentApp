d22d4a111f2faae613ddc53b49460471d97909da
ygladiensyah <ygladiensyah@gmail.com>
2019-04-12 22:03:49 +0700

    fix login activity

--- app/src/main/java/com/nusantarian/eggshellentapp/LoginActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/LoginActivity.java
@@ -1,13 +1,69 @@
 package com.nusantarian.eggshellentapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
+    private EditText et_username, et_password;
+    private FirebaseAuth mAuth;
+
+    SignInButton btn_google;
+    private static final int RC_SIGN_IN = 123;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
+
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        TextView tv_forgot = findViewById(R.id.tv_forgot);
+        Button btn_login = findViewById(R.id.btn_login);
+        Button btn_signup = findViewById(R.id.btn_signup);
+        Button button = findViewById(R.id.btn_google);
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+
+        tv_forgot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgotActivity.class));
+                finish();
+            }
+        });
     }
 }
