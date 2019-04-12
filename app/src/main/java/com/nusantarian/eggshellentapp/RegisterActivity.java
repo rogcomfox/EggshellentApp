1940fae030eba1ab2f0f3647f02b0fcebacb3e29
ygladiensyah <ygladiensyah@gmail.com>
2019-04-13 05:48:14 +0700

    fix register

--- app/src/main/java/com/nusantarian/eggshellentapp/RegisterActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/RegisterActivity.java
@@ -1,13 +1,36 @@
 package com.nusantarian.eggshellentapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText et_name, et_email, et_phone, et_password, et_confpass;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btn_signup = findViewById(R.id.btn_signup);
+        TextView tv_login = findViewById(R.id.tv_login);
+
+        tv_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
