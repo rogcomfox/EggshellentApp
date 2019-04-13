485c3e6d51362160573426e0e7289479a6bf21db
ygladiensyah <ygladiensyah@gmail.com>
2019-04-13 09:45:13 +0700

    fix main activity

--- app/src/main/java/com/nusantarian/eggshellentapp/MainActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.nusantarian.eggshellentapp;
 
+import android.content.ClipData;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+       bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               switch (menuItem.getItemId()){
+                   case R.id.icon_home: return true;
+                   case R.id.icon_order: return true;
+                   case R.id.icon_profil: return true;
+               }
+               return false;
+           }
+       });
     }
 }
