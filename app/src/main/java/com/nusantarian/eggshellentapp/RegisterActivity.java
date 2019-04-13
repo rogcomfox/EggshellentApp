0f71ffe537d3c4ebe45cca3dac43bc3e3068ceae
ygladiensyah <ygladiensyah@gmail.com>
2019-04-13 07:43:13 +0700

    fix register

--- app/src/main/java/com/nusantarian/eggshellentapp/RegisterActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/RegisterActivity.java
@@ -1,19 +1,26 @@
 package com.nusantarian.eggshellentapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText et_name, et_email, et_phone, et_password, et_confpass;
+    private EditText et_email, et_password;
     private FirebaseAuth mAuth;
     private static final String TAG = "";
 
@@ -22,7 +29,6 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button btn_signup = findViewById(R.id.btn_signup);
         TextView tv_login = findViewById(R.id.tv_login);
 
         tv_login.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +36,41 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 finish();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+        Button btn_signup = findViewById(R.id.btn_signup);
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
+
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText().toString();
+                String password = et_password.getText().toString();
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter Eamil Id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "Create Account Success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            finish();
+                        }else{
+                            Log.w(TAG, "Create Account Failed", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
