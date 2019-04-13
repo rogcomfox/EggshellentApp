986c853b0ec5899cf96848568f212be6b7775513
ygladiensyah <ygladiensyah@gmail.com>
2019-04-13 08:07:43 +0700

    fix forgot activity

--- app/src/main/java/com/nusantarian/eggshellentapp/ForgotActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/ForgotActivity.java
@@ -1,13 +1,62 @@
 package com.nusantarian.eggshellentapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotActivity extends AppCompatActivity {
+
+    private EditText et_forgot;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot);
+
+        et_forgot = findViewById(R.id.et_forgot);
+        Button btn_reset = findViewById(R.id.btn_reset);
+        TextView tv_back = findViewById(R.id.tv_back);
+
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ForgotActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_forgot.getText().toString();
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(ForgotActivity.this, "Pelase Enter Your Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(ForgotActivity.this, "Please Check Your Email", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(ForgotActivity.this, "Failed To Reset Password", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
