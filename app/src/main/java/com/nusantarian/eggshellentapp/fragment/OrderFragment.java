65216ff1818935692c41ba9e411e8cbf11278b3f
ygladiensyah <ygladiensyah@gmail.com>
2019-04-23 23:56:45 +0700

    fix order fragment

--- app/src/main/java/com/nusantarian/eggshellentapp/fragment/OrderFragment.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/fragment/OrderFragment.java
@@ -1,28 +1,43 @@
 package com.nusantarian.eggshellentapp.fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.nusantarian.eggshellentapp.activity.OrderMenuActivity;
 import com.nusantarian.eggshellentapp.R;
+
+import java.util.Objects;
 
 
 public class OrderFragment extends Fragment{
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_order, container, false);
+    public OrderFragment(){
+
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_order, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        CardView cv_order = view.findViewById(R.id.cv_order);
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
+
+        cv_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), OrderMenuActivity.class));
+            }
+        });
+
+        return view;
     }
 }
