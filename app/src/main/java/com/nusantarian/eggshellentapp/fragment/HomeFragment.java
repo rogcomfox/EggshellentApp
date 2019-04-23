fb2b97356e50ba9a77c18843d9a73034972e945a
ygladiensyah <ygladiensyah@gmail.com>
2019-04-23 23:56:27 +0700

    fix home fragment

--- app/src/main/java/com/nusantarian/eggshellentapp/fragment/HomeFragment.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/fragment/HomeFragment.java
@@ -1,26 +1,60 @@
 package com.nusantarian.eggshellentapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.nusantarian.eggshellentapp.activity.AboutActivity;
 import com.nusantarian.eggshellentapp.R;
+
+import java.util.Objects;
 
 
 public class HomeFragment extends Fragment{
 
+    public HomeFragment(){
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
+        return view;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.menu_home_toolbar, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.icon_search:
+                Toast.makeText(getActivity(), "Search Under Construction", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.icon_about:
+                startActivity(new Intent(getContext(), AboutActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
