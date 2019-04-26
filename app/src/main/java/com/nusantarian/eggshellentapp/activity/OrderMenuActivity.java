533444109ac566416b6182e8e041d5e13a29504d
ygladiensyah <ygladiensyah@gmail.com>
2019-04-26 19:41:45 +0700

    order menu

---
+++ app/src/main/java/com/nusantarian/eggshellentapp/activity/OrderMenuActivity.java
@@ -0,0 +1,15 @@
+package com.nusantarian.eggshellentapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nusantarian.eggshellentapp.R;
+
+public class OrderMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_menu);
+    }
+}
