1a695f33757c06a0484cc1e7528c3e9985e7cd7e
ygladiensyah <ygladiensyah@gmail.com>
2019-04-26 21:51:12 +0700

    order menu fix

--- app/src/main/java/com/nusantarian/eggshellentapp/activity/OrderMenuActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/activity/OrderMenuActivity.java
@@ -1,7 +1,11 @@
 package com.nusantarian.eggshellentapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.nusantarian.eggshellentapp.R;
 
@@ -11,5 +15,27 @@ public class OrderMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_menu);
+        ImageView img_1 = findViewById(R.id.img_1);
+        ImageView img_2 = findViewById(R.id.img_2);
+        ImageView img_3 = findViewById(R.id.img_3);
+
+        img_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BreadActivity.class));
+            }
+        });
+        img_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Please Select Another Product", Toast.LENGTH_LONG).show();
+            }
+        });
+        img_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Please Select Another Product", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
