d9e87aa7bdbce7130de2d886abe8a9ea91f632f0
ygladiensyah <ygladiensyah@gmail.com>
2019-04-26 19:42:06 +0700

    about activity

--- app/src/main/java/com/nusantarian/eggshellentapp/activity/AboutActivity.java
+++ app/src/main/java/com/nusantarian/eggshellentapp/activity/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.nusantarian.eggshellentapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.nusantarian.eggshellentapp.R;
 
@@ -11,5 +14,14 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ImageView img_back = findViewById(R.id.img_back);
+
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
     }
 }
